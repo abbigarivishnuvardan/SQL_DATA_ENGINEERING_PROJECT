8630c940815e9f807e44fac00a5e7e3a39e2e6d2
ABBIGARI VISHNU VARDHAN <153485405+abbigarivishnuvardan@users.noreply.github.com>
2026-09-06 20:17:31 +0530

    IT'S MY UNDERSTANDING
    Added a comment indicating file editing.

--- lessons/1.9/1.9/1.9/visual_intro.sql
+++ lessons/1.9/1.9/1.9/visual_intro.sql
@@ -3,3 +3,4 @@ select job_id,job_title_short,salary_year_avg,company_id from job_postings_fact 
 use data_jobs;
 select job_id,job_title_short,salary_year_avg,company_id from job_postings_fact limit 10;
 select * from job_postings_fact limit 10;
+IAM EDITING THESE FILE
